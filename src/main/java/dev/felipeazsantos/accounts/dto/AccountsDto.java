f9eb820bb8e1ab2ab79d54b10c8489cabee4cb51
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-11 14:19:09 -0300

    validation dtos

--- src/main/java/dev/felipeazsantos/accounts/dto/AccountsDto.java
+++ src/main/java/dev/felipeazsantos/accounts/dto/AccountsDto.java
@@ -1,11 +1,19 @@
 package dev.felipeazsantos.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp = "($|[0-9]{10})", message = "AccountNumber must be 10 digits")
     private Long accountNumber;
-    private String AccountType;
+
+    @NotEmpty(message = "AccountType cannot be a null or empty")
+    private String accountType;
+
+    @NotEmpty(message = "BranchAddress cannot be a null or empty")
     private String branchAddress;
 }
