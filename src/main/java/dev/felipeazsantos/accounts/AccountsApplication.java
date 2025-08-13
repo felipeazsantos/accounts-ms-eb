3658ee920148045a0222cc5857520368dda32b9d
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-13 20:04:51 -0300

    swagger open api document api accounts

--- src/main/java/dev/felipeazsantos/accounts/AccountsApplication.java
+++ src/main/java/dev/felipeazsantos/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package dev.felipeazsantos.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Felipe Azevedo",
+                        email = "test@email.com",
+                        url = "https://github.com/felipeazsantos"
+                ),
+                license = @License(
+                    name = "Apache 2.0",
+                    url = "https://github.com/felipeazsantos"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Accounts microservice REST API Documentation",
+                url = "https://github.com/felipeazsantos"
+        )
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
